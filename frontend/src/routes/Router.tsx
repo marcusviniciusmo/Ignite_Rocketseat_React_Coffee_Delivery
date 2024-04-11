175b9042e4178085dc0822984f64a337e32201c0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-11 17:12:38 +0100

    Success component created

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Checkout } from '../components/Checkout';
+import { Success } from '../components/Success';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/checkout" element={<Checkout />} />
+      <Route path="/success" element={<Success />} />
     </Routes>
   );
 }
