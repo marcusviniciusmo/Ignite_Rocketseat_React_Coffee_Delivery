7068fed262dbe13940a28d0de94015ebd133122f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-11 16:33:02 +0100

    Default theme configuration

---
+++ frontend/src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  primaryColor: '#DBAC2C',
+  primaryColorDark: '#C47F17',
+  primaryColorLight: '#F1E9C9',
+
+  secondaryColor: '#8047F8',
+  secondaryColorDark: '#4B2995',
+  secondaryColorLight: '#EBE5F9',
+
+  baseTitle: '#272221',
+  baseSubtitle: '#403937',
+  baseText: '#574F4D',
+  baseLabel: '#8D8686',
+  baseHover: '#D7D5D5',
+
+  baseButton: '#E6E5E5',
+  baseInput: '#EDEDED',
+  baseCard: '#F3F2F2',
+  background: '#FAFAFA',
+  white: '#FFFFFF',
+};
