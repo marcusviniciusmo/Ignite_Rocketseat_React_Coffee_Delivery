5105f7a37073ac627f5daa71f795096aee2f8add
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-11 17:17:28 +0100

    Routes layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components';
-import { Header } from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './routes/Router';
 import { GlobalStyle } from './styles/global';
@@ -8,7 +7,6 @@ import { defaultTheme } from './styles/themes/default';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Header />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from '../layout/DefaultLayout';
 import { Home } from '../pages/Home';
 import { Checkout } from '../components/Checkout';
 import { Success } from '../components/Success';
@@ -6,9 +7,11 @@ import { Success } from '../components/Success';
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/success" element={<Success />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
     </Routes>
   );
 }
