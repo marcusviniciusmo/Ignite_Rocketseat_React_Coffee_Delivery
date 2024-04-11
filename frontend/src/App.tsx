76497f7280103099783da146dfb83c5d5de20aa9
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-11 17:06:34 +0100

    Home page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
+import { Router } from './routes/Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
-import { Router } from './routes/Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <h1>Coffee Delivery</h1>
         <Router />
       </BrowserRouter>
       <GlobalStyle />
