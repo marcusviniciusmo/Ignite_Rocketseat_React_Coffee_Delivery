38c0343075b1a52777b75d2ed3f316ec1242d5f4
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-11 17:02:55 +0100

    Router file created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
+import { Router } from './routes/Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Coffee Delivery</h1>
+      <BrowserRouter>
+        <h1>Coffee Delivery</h1>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
