9a9e5108f74ec5f8e66565349c4ab0f79a9f17b5
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-12 06:56:57 +0100

    CoffeeList component created

---
+++ frontend/src/components/CoffeeList/index.tsx
@@ -0,0 +1,7 @@
+export function CoffeeList() {
+  return (
+    <div>
+      <h1>COFFEE LIST</h1>
+    </div>
+  );
+}
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Intro } from '../../components/Intro';
+import { CoffeeList } from '../../components/CoffeeList';
 
 export function Home() {
   return (
     <div>
       <Intro />
+      <CoffeeList />
     </div>
   );
 }
