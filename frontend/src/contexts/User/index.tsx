1dff57fc558815b3a7d8104d5e9d556be60bc61d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-22 08:29:11 +0100

    Getting address by cep

--- frontend/src/contexts/User/index.tsx
+++ frontend/src/contexts/User/index.tsx
@@ -1,4 +1,10 @@
-import { ChangeEvent, ReactNode, createContext, useState } from 'react';
+import {
+  ChangeEvent,
+  ReactNode,
+  createContext,
+  useEffect,
+  useState,
+} from 'react';
 import { Address } from '../../@types/Address';
 
 interface UserContextType {
@@ -25,6 +31,29 @@ export function UserContextProvider({ children }: UserContextProps) {
     localidade: '',
     uf: '',
   });
+
+  useEffect(() => {
+    if (userAddress.cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${userAddress.cep}/json/`)
+        .then((response) => response.json())
+        .then((data) => {
+          const mappedData: Address = {
+            cep: data.cep,
+            logradouro: data.logradouro,
+            numero: '',
+            complemento: data.complemento,
+            bairro: data.bairro,
+            localidade: data.localidade,
+            uf: data.uf,
+          };
+
+          setUserAddress(mappedData);
+        })
+        .catch((error) => {
+          console.error('Erro ao buscar dados do CEP', error);
+        });
+    }
+  }, [userAddress.cep]);
 
   function onClickSetPaymentType(type: string) {
     setPaymentType(type);
